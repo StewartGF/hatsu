4c0e24ece2bcea7ac99ff88d54188d2176f089d1
Stewart Granger Flores <stewart.grngrf@gmail.com>
2020-08-02 19:57:07 -0400

    modificación target_blank 🧙🏻‍♂️

--- src/components/ApisContainer/Api.js
+++ src/components/ApisContainer/Api.js
@@ -5,37 +5,39 @@ const Apis = (props) => {
   return (
     <>
       <div className="w-full border shadow-lg rounded p-4 ">
-        <div id="top-container" className="text-xl flex align-center">
-          <div id="imageContainer" className="w-1/5 flex mx-auto">
-            <img
-              className="w-10  h-10 rounded-full mx-auto"
-              src={data.imageUrl}
-              alt="logo"
-            />
+        <a href={data.url} target="_blank" rel="noopener noreferrer">
+          <div id="top-container" className="text-xl flex align-center">
+            <div id="imageContainer" className="w-1/5 flex mx-auto">
+              <img
+                className="w-10  h-10 rounded-full mx-auto"
+                src={data.imageUrl}
+                alt="logo"
+              />
+            </div>
+            <div
+              id="nameContainer"
+              className="w-4/5 flex items-center font-black tracking-tighter justify-start"
+            >
+              {data.name}
+            </div>
           </div>
-          <div
-            id="nameContainer"
-            className="w-4/5 flex items-center font-black tracking-tighter justify-start"
-          >
-            {data.name}
+          <div id="bottom-container" className="  mt-4">
+            <p>{data.description}</p>
+            <div className="px-2 py-4">
+              {data.tags &&
+                data.tags.map((tag) => {
+                  return (
+                    <span
+                      className="inline-block bg-red-100 rounded-full px-3 py-1 text-xs xl:text-sm  font-semibold text-gray-700  border-2 "
+                      key={tag}
+                    >
+                      #{tag}
+                    </span>
+                  );
+                })}
+            </div>
           </div>
-        </div>
-        <div id="bottom-container" className="  mt-4">
-          <p>{data.description}</p>
-          <div className="px-2 py-4">
-            {data.tags &&
-              data.tags.map((tag) => {
-                return (
-                  <span
-                    className="inline-block bg-red-100 rounded-full px-3 py-1 text-xs xl:text-sm  font-semibold text-gray-700  border-2 "
-                    key={tag}
-                  >
-                    #{tag}
-                  </span>
-                );
-              })}
-          </div>
-        </div>
+        </a>
       </div>
     </>
   );
