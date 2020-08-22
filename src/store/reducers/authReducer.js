341ec770df33d41c5eeb1434880ca9ed32a40607
Stewart Granger Flores <stewart.grngrf@gmail.com>
2020-08-22 19:42:10 -0400

    Changing authReducer Redux🚀

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,5 +1,19 @@
-const initialState = {};
+import storage from "local-storage-fallback";
+
+const initialState = {
+  user: storage.getItem("user") ? JSON.parse(storage.getItem("user")) : false,
+};
 const authReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "SET_USER":
+      storage.setItem("user", JSON.stringify(action.payload));
+      return { ...state, user: { ...state.user, ...action.payload } };
+    // state.user = action.payload;
+    // console.log("new state", state.user);
+    case "LOGOUT":
+      return { ...state, user: false };
+    default:
+      return state;
+  }
 };
 export default authReducer;
